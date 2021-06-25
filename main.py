ae82558d61bc7685264776ea8889ae36b957c6c5
YarMolenko13 <65036611+YarMolenko13@users.noreply.github.com>
2021-06-25 12:42:43 +0300

    start

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ bear_bgr = cv2.imread('img/test.png')
 
 bear_bgr_median = cv2.medianBlur(bear_bgr, 5)
 bear_hsv_median = cv2.cvtColor(bear_bgr_median, cv2.COLOR_BGR2GRAY)
-_, thr = cv2.threshold(bear_hsv_median, 100, 255, cv2.THRESH_OTSU)
-# thr_adpt = cv2.adaptiveThreshold(bear_hsv_median, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 25)
+_, thr = cv2.threshold(bear_hsv_median, 0, 255, cv2.THRESH_OTSU)  # подбирает автоматически
+# thr_adpt = cv2.adaptiveThreshold(bear_hsv_median, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 121, 25)
 
 
 # cv2.imshow('picture1',bear_bgr[:,:,0])
-# cv2.imshow('pic', thr_adpt)
-cv2.imshow('picture2', thr  )
+cv2.imshow('pic', thr)
+# cv2.imshow('picture2', thr  )
 cv2.waitKey(0)
-
-# if __name__ == '__main__':
