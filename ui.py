7a6bca766f528e67da071241858758abadfe8105
YarMolenko13 <65036611+YarMolenko13@users.noreply.github.com>
2021-06-28 11:55:11 +0300

    ярослав ui готов

--- ui.py
+++ ui.py
@@ -8,14 +8,14 @@ orig_photo_dir = 'D:/Projects/Python/PolarBear/img/test.png'
 root = Tk()
 root.configure(background = "#B0BA84")
 root.title('Polar bear')
-# root.geometry("550x300+300+150")
-root.geometry("200x70")
+# root.geometry("550x300+500+350")
+root.geometry("200x70+650+300")
 root.resizable(width=True, height=True)
 
 def open_img():
     filename = filedialog.askopenfilename(title='open')
     img = Image.open(filename)
-    root.geometry("700x550")
+    root.geometry("700x550+390+100")
     img = img.resize((450, 450), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
     panel = Label(root, image=img, pady=40)
