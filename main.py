fae438a01108ab70766f8fbfa872a507cf85bf92
YarMolenko13 <65036611+YarMolenko13@users.noreply.github.com>
2021-06-30 14:06:57 +0300

    ярослав

--- main.py
+++ main.py
@@ -1,11 +1,93 @@
 import cv2
+import numpy as np
+from PIL import *
+from sklearn.cluster import KMeans
+from collections import Counter
+from matplotlib import pyplot as plt
 
 def find_bear(path):
-    bear_bgr = cv2.imread(path)
-    bear_bgr_median = cv2.medianBlur(bear_bgr, 5)
-    bear_hsv_median = cv2.cvtColor(bear_bgr_median, cv2.COLOR_BGR2GRAY)
-    _, thr = cv2.threshold(bear_hsv_median, 0, 255, cv2.THRESH_OTSU)
-    photot_name = path.split('/')[-1]
+    img = cv2.imread(path)
+    img_median = cv2.medianBlur(img, 5)
+    img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_hsv = cv2.cvtColor(img_median, cv2.COLOR_BGR2HSV)
+    img_hsv = img_hsv[:,:,0]
+    # cv2.imshow('pic', img_hsv)
+
+    # img_hsv[0] = 110
+
+    # _, thr = cv2.threshold(img_hsv, 0, 255, cv2.THRESH_OTSU)
+    thr = cv2.adaptiveThreshold(img_hsv, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 51, 30)
+
+    # cv2.imshow('pic', thr)
+
+    def get_domination_color(image, k=1):
+        image = image.reshape((image.shape[0] * image.shape[1], -1))
+
+        clt = KMeans(n_clusters=k)
+        labels = clt.fit_predict(image)
+
+        label_counts = Counter(labels)
+
+        dominant_color = clt.cluster_centers_[label_counts.most_common(1)[0][0]]
+
+        return list(dominant_color)
+
+    is_bear_founded = False
+    count = 0
+    # while not is_bear_founded:
+    while count <= 15:
+        h = 50
+        w = 50
+        position = np.unravel_index(np.argmax(thr), thr.shape)
+        y = position[0] - 25
+        x = position[1] - 25
+        # cv2.rectangle(thr, (x, y), (x + w, y + h), (0, 0, 0), -1)
+
+        thr_crop = thr[y:y+50, x:x+50]
+
+
+
+
+        color = get_domination_color(thr_crop)
+        print(color)
+
+        count = 16
+
+        count+=1
+        is_bear_founded = True
+
+    cv2.imshow('pic', thr_crop)
+    # plt.imshow(thr_crop)
+    # plt.show()
+
+
+
+
+
+
+
+    # light_orange = (50, 10, 20)
+    # dark_orange = (60, 20, 80)
+    # mask = cv2.inRange(thr, (0, 0, 0), (100, 100, 100))
+    # cv2.imshow('pic',mask)
+
+    # position = np.unravel_index(np.argmax(thr), thr.shape)
+    # print(position)
+    # закрашивать часть
+    # y = position[0] - 100
+    # x = position[1] - 100
+    # h = 200
+    # w = 200
+    # crop = img_RGB[y:y + h, x:x + w]
+    # print(position)
+    #
+    # cv2.rectangle(img_RGB, (x, y), (x + w, y + h), (0, 255, 255), 10)
+    # plt.imshow(img_RGB)
+    # plt.show()
+
+    cv2.waitKey(0)
+
+
 
 find_bear('D:/Projects/Python/PolarBear/img/test.png')
 
--- ui.py
+++ ui.py
@@ -1,25 +1,46 @@
 from tkinter import *
+from tkinter.ttk import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 from main import *
 
+# style = Style()
+# style.configure("BW.Tlabel", foreground=)
+
 root = Tk()
-root.configure(background = "#B0BA84")
+root.configure(background="#B0BA84")
 root.title('Polar bear')
 # root.geometry("550x300+500+350")
-root.geometry("200x70+650+300")
+root.geometry("300x70+650+300")
 root.resizable(width=True, height=True)
+
 
 def open_img():
     filename = filedialog.askopenfilename(title='open')
+    # foldername = filedialog.askdirectory(title='open')
+    # print(foldername)
     img = Image.open(filename)
-    root.geometry("700x550+390+100")
+    root.geometry("700x570+390+100")
     img = img.resize((450, 450), Image.ANTIALIAS)
     img = ImageTk.PhotoImage(img)
-    panel = Label(root, image=img, pady=40)
+    panel = Label(root, image=img, padding=(0,10,0,0), background="#B0BA84")
     panel.image = img
     panel.pack()
 
-btn = Button(root, text="Выбери фото", bg="#266E67", fg="#fff", command=open_img).pack(pady=20)
+def open_folder():
+    filename = filedialog.askopenfilename(title='open')
+
+# btn = Button(root, text="Выбери фото", bg="#266E67", fg="#fff", command=open_img)
+frame_btn = Label(root, background="#B0BA84")
+# frame_btn.configure(bg="#B0BA84", pady=20)
+frame_btn.pack(pady=20)
+
+btn = Button(frame_btn, text="Выберите фото", command=open_img)
+btn2 = Button(frame_btn, text="Выберите папку", command=open_folder)
+empty = Label(frame_btn, background="#B0BA84", width=5)
+btn.pack(side="left")
+empty.pack(side="left")
+btn2.pack(side="right")
+
 
 root.mainloop()
